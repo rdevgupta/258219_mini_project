0e49e8cda745331cadd455563a956645b341ebeb
Dev Gupta <guptadev359@gmail.com>
2021-04-25 15:52:35 +0530

    added func to delete an employee or complete data file

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+import os
 #function for adding data of the employee to the file
 def AddData():
     print("Enter the Following details about the employee please: ")
     sfid = input("Enter the SFID of Employee: \n")
-    # #seperate file containing sfid only for ease in searching a particular employee
-    # f = open("EmpSFID's.txt", "at")
-    # f.write(sfid+"\n")
-    # f.close()
     name = input("Enter the name of Employee: \n")
     email = input("Enter email id: \n")
     designation = input("Position in the company: \n")
@@ -38,7 +35,25 @@ def SearchEmployee(sfid):
         print("ERROR: Employee not Found with the entered sfid!")
     file.close()
 
+#to delete particular employee
+def DeleteEmployee(sfid):
+    file = open("EmployeeData.txt", "r")
+    content = file.readlines()
+    file.close()
     
+    new_file = open("EmployeeData.txt","w")
+    for line in content:
+        if line[0:6] != sfid:
+            new_file.write(line)
+    new_file.close()
+
+#delete the entire data file permenantly
+def DeleteData():
+    print("Data is permenantly deleted...")
+    if os.path.exists("EmployeeData.txt"):
+        os.remove("EmployeeData.txt")
+    else:
+        print("The file does not exist")
 #main driver code or menu of the system
 
 print('''
@@ -50,9 +65,13 @@ print('''
 
                           2. VIEW DATA
 
-                          3. SEARCH EMPLOYEE     
+                          3. SEARCH EMPLOYEE  
 
-                          4. EXIT                          ''')
+                          4. DELETE DATA OF AN EMPLOYEE   
+
+                          5. DELETE ENTIRE DATA
+
+                          6. EXIT                          ''')
 
 choice = int(input("What you want to do with database, make a choice: "))
 
@@ -69,6 +88,19 @@ while choice != 0:
         sfid_str = input()
         SearchEmployee(sfid_str)
     elif choice == 4:
+        # delete entry of employee
+        print("Enter the SFID of the employee whose data to be deleted")
+        sfid_del = input()
+        DeleteEmployee(sfid_del)
+        print("Data deleted Successfully!....\n")
+    elif choice == 5:
+        print("Entire data will be lost forever.... are you sure to do this? 'y' for yes and 'n' for no: \n")
+        confirmation = input()
+        if confirmation == 'y':
+            DeleteData()
+        else:
+            print("Okay! Data not deleted...")
+    elif choice == 6:
         print("Thankyou See You Soon.....Buddy!")
         exit()
     else:
